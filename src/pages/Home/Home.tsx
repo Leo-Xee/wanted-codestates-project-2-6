d16234647a82e99a38908bfc25951e47e2c24b42
Kae <kaehehehe@gmail.com>
2022-03-10 16:24:50 +0900

    Feat: 홈 화면 구현

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,26 @@
 import React from "react";
 
+import * as S from "./style";
+
 type HomeProps = {
   setRoute: React.Dispatch<React.SetStateAction<string>>;
 };
 
 function Home({ setRoute }: HomeProps) {
-  return <div>Home</div>;
+  const handleClickBtn = () => {
+    setRoute("apply");
+  };
+  return (
+    <S.Container>
+      <S.Title>
+        어르신 돌보미
+        <br />
+        <strong>케어코디</strong>
+      </S.Title>
+      <h1></h1>
+      <S.Button onClick={handleClickBtn}>신청하기</S.Button>
+    </S.Container>
+  );
 }
 
 export default Home;
