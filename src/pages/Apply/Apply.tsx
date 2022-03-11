01103fbb98839abb7719206286b9cedea999a035
Parksangwoo <psu8430@gmail.com>
2022-03-11 14:31:26 +0900

    Feat: Check Validate about 3단계

--- src/pages/Apply/Apply.tsx
+++ src/pages/Apply/Apply.tsx
@@ -23,7 +23,7 @@ function Apply({ setRoute }: ApplyProps) {
     >
       {step === 1 && <CareType setDisabled={setDisabled} />}
       {step === 2 && <CareSchedule setDisabled={setDisabled} />}
-      {step === 3 && <CarePlace />}
+      {step === 3 && <CarePlace setDisabled={setDisabled} />}
       {step === 4 && <RegisterAddress setRoute={setRoute} />}
       {step === 5 && <div>다섯번째 스텝</div>}
     </ApplyTemplate>
