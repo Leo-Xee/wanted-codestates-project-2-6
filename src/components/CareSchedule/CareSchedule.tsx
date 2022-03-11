ac3cfa7a00cc9c640958d7960fd9be709f4c008f
jinn2u <bsybear623@gmail.com>
2022-03-11 13:56:58 +0900

    feat: context 전송

--- src/components/CareSchedule/CareSchedule.tsx
+++ src/components/CareSchedule/CareSchedule.tsx
@@ -1,7 +1,8 @@
-import React, { useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import Time from "../Time";
 import Calendar from "../Calendar";
-
+import { useApplicationDispatch } from "src/contexts/ApplicationContext";
+import { useApplicationState } from "src/contexts/ApplicationContext";
 const Schedule = ({
   setDisabled,
 }: {
@@ -9,11 +10,51 @@ const Schedule = ({
 }) => {
   const [pickedStartDate, setPickedStartDate] = useState("");
   const [pickedEndDate, setPickedEndDate] = useState("");
+  const [visitHour, setVisitHour] = useState("선택");
+  const [careHours, setCareHours] = useState("선택");
   const isToday = useMemo(() => {
     const today = new Date().getDate();
     const pickedDate = new Date(pickedStartDate).getDate();
     return today === pickedDate;
   }, [pickedStartDate]);
+  const dispatch = useApplicationDispatch();
+  const applicationState = useApplicationState();
+  useEffect(() => {
+    if (
+      applicationState.workType === "24시간 상주" &&
+      pickedStartDate.length &&
+      pickedEndDate.length &&
+      visitHour !== "선택"
+    ) {
+      dispatch({
+        type: "SET_SCHEDULE",
+        schedule: {
+          startDate: pickedStartDate,
+          endDate: pickedEndDate,
+          visitTime: visitHour,
+          hour: null,
+        },
+      });
+      return setDisabled(false);
+    }
+    if (
+      pickedStartDate.length &&
+      pickedEndDate.length &&
+      visitHour !== "선택" &&
+      careHours !== "선택"
+    ) {
+      dispatch({
+        type: "SET_SCHEDULE",
+        schedule: {
+          startDate: pickedStartDate,
+          endDate: pickedEndDate,
+          visitTime: visitHour,
+          hour: +careHours[0],
+        },
+      });
+      setDisabled(false);
+    }
+  }, [pickedStartDate, pickedEndDate, visitHour, careHours, dispatch, setDisabled]);
 
   return (
     <>
@@ -23,7 +64,13 @@ const Schedule = ({
         setPickedStartDate={setPickedStartDate}
         setPickedEndDate={setPickedEndDate}
       />
-      <Time isToday={isToday} />
+      <Time
+        isToday={isToday}
+        visitHour={visitHour}
+        setVisitHour={setVisitHour}
+        careHours={careHours}
+        setCareHours={setCareHours}
+      />
     </>
   );
 };
