cd89f7d7af9034cf1d726d45c5ea36c05c9509c7
Kae <kaehehehe@gmail.com>
2022-03-11 10:12:10 +0900

    Fix: 컴포넌트 이름을 통일성있게 변경 (Schedule -> CareSchedule)

--- src/pages/Apply/Apply.tsx
+++ src/pages/Apply/Apply.tsx
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
 import RegisterAddress from "src/components/RegisterAddress/RegisterAddress";
-import Schedule from "src/components/Schedule";
+import CareSchedule from "src/components/CareSchedule/CareSchedule";
 import ApplyTemplate from "../../components/ApplyTemplate";
 import CarePlace from "../../components/CarePlace";
 import CareType from "../../components/CareType";
-import Time from "../../components/Time/Time";
 
 type ApplyProps = {
   setRoute: React.Dispatch<React.SetStateAction<string>>;
@@ -16,7 +15,7 @@ function Apply({ setRoute }: ApplyProps) {
   return (
     <ApplyTemplate setRoute={setRoute} step={step} setStep={setStep}>
       {step === 1 && <CareType />}
-      {step === 2 && <Schedule />}
+      {step === 2 && <CareSchedule />}
       {step === 3 && <CarePlace />}
       {step === 4 && <RegisterAddress setRoute={setRoute} />}
       {step === 5 && <div>다섯번째 스텝</div>}
