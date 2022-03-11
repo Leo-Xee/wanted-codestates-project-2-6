16aa939e94768538488ca23508190bd608b324fb
jinn2u <bsybear623@gmail.com>
2022-03-11 14:48:51 +0900

    feat: calendar 스타일 변경

--- src/components/Calendar/style.ts
+++ src/components/Calendar/style.ts
@@ -1,26 +1,32 @@
 import styled from "styled-components";
 
 export const H3 = styled.h3`
-  font-size: 14px;
+  font-size: 15px;
   font-weight: bold;
   margin-bottom: 10px;
 `;
 
 export const Input = styled.input`
   width: 100%;
-  height: 60px;
-  padding: 0px 5px;
+  height: 48px;
+  padding: 0px 5px 0px 16px;
   text-align: left;
-  font-size: 16px;
+  font-size: 14px;
   font-weight: bold;
   color: lightgray;
   border: none;
   background: #f6f6f6;
+  &:valid {
+    font-weight: normal;
+    color: black;
+  }
 `;
-export const InpWrapper = styled.div`
-  width: 100%;
-  height: 60px;
-  background: #f6f6f6;
-  border-radius: 4px;
-  margin-bottom: 10px;
+
+export const Wrapper = styled.div`
+  display: flex;
+  width: calc(100% - 10px);
+  gap: 10px;
+`;
+export const Container = styled.div`
+  width: 50%;
 `;
